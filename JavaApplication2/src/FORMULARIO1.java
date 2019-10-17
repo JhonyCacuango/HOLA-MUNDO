a1c120a3b29d124ebf89baa04a5bae3d6e78aa17
JhonyCacuango <38759809+JhonyCacuango@users.noreply.github.com>
2019-10-17 08:15:31 -0500

    Ventana2 ingreso a nueva ventana

--- JavaApplication2/src/FORMULARIO1.java
+++ JavaApplication2/src/FORMULARIO1.java
@@ -81,8 +81,8 @@ public class FORMULARIO1 extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         
-        Ventana2 ven = new Ventana2();
-        ven.setVisible(true);
+        //Ventana2 ven = new Ventana2();
+        //ven.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
